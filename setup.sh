a135807aea04fbab91159bb4efcb13942577ca7e
Timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2016-03-25 16:30:28 -0300

    .

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 ## Author: Timoshenko
 ## Objective: Setup a default environment (well, at leats for me =D ) using CentOS
-echo "Do you want to update your system?"
-sudo yum update
-echo "Do you want to install all Development Tools into your system?"
-sudo yum groupinstall "Development Tools"
-echo "Do you want to install some features into your system?"
-sudo yum install lynx net-tools* ncurse* bc java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless wget
+echo "Update your system."
+sudo yum update -y
+echo "Install all Development Tools into your system."
+sudo yum groupinstall "Development Tools" -y
+echo "Install some features into your system."
+sudo yum install lynx net-tools* ncurse* bc java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless wget -y
 echo "Trying to install HTOP tool:"
 cd && mkdir softs_devel && cd softs_devel/ && wget -c "http://hisham.hm/htop/releases/1.0.3/htop-1.0.3.tar.gz" && tar -zxvf htop-1.0.3.tar.gz && cd htop-1.0.3/ && ./configure --prefix=/usr && make && sudo make install
 cd ..
