d86b7b7b5422640a79a410f574cc43db6b0e24b5
Timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2016-10-08 09:23:07 -0300

    update the htop version

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo yum groupinstall "Development Tools" -y
 echo "Install some features into your system."
 sudo yum install lynx net-tools* ncurse* bc java-1.8.0-openjdk java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless wget -y
 echo "Trying to install HTOP tool:"
-cd && mkdir softs_devel && cd softs_devel/ && wget -c "http://hisham.hm/htop/releases/1.0.3/htop-1.0.3.tar.gz" && tar -zxvf htop-1.0.3.tar.gz && cd htop-1.0.3/ && ./configure --prefix=/usr && make && sudo make install
+cd && mkdir softs_devel && cd softs_devel/ && wget -c "http://hisham.hm/htop/releases/2.0.2/htop-2.0.2.tar.gz" && tar -zxvf htop-2.0.2.tar.gz && cd htop-2.0.2/ && ./configure --prefix=/usr && make && sudo make install
 cd ..
 echo "Trying to install NCDU tool:"
 wget -c "http://dev.yorhel.nl/download/ncdu-1.11.tar.gz" && tar -zxvf ncdu-1.11.tar.gz && cd ncdu-1.11/ && ./configure --prefix=/usr && make && sudo make install
