c28350765f85f45872e4d8dcf29ca0a943042e84
Timoshenko <softctrl@users.noreply.github.com>
2017-12-12 11:13:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 ## Author: Timoshenko
 ## Objective: Setup a default environment (well, at leats for me =D ) using CentOS
 echo "Enable EPEL repository on Centos7"
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-sudo rpm -ivh epel-release-7-9.noarch.rpm
+sudo yum install epel-release
 echo "Update your system."
 sudo yum update -y
 echo "Install all Development Tools into your system."
